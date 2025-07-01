fb6fd4630a71e9dd02e350a4bd2588b759b64a8c
AndraCristiana07 <taporandra@gmail.com>
2025-07-01 17:29:57 +0100

    change timer to be able to give random piece in tetris

--- timer.go
+++ timer.go
@@ -37,15 +37,16 @@ func (t *Timer) Write(address uint16, value byte) {
 }
 
 func (t *Timer) Update(tCycles int, cpu *CPU) {
-	//div -ncremented every 256 tCycles
+
 	t.div += uint16(tCycles)
-	if t.div >= 256 {
-		t.div -= 256
-	}
+	cpu.Memory[0xFF04] = byte(t.div >> 8)
+	//if t.div >= 256 {
+	//	t.div -= 256
+	//}
 	if t.TAC&(1<<2) == 0 { //timer disabled
 		return
 	}
-	var rate int
+	var rate uint16
 	// Bits 1-0 : Clock Select
 	switch t.TAC & 0b0011 {
 	case 0b00:
@@ -57,17 +58,60 @@ func (t *Timer) Update(tCycles int, cpu *CPU) {
 	case 0b11:
 		rate = 256
 	}
-	if t.TAC&(1<<2) != 0 { //timer enabled
-		currDIVBit := t.div & uint16(rate)
 
-		if currDIVBit == 1 && t.oldStoredDivBit == 0 { //"rising edge" (meaning going from 0 to 1)
+	// bit that controls the timer inc
+	bitPos := map[uint16]uint8{
+		1024: 9,
+		16:   3,
+		64:   6,
+		256:  8,
+	}[rate]
+
+	if t.TAC&(1<<2) != 0 { //timer enabled
+		currDIVBit := (t.div >> bitPos) & 1
+		prevBit := (t.oldStoredDivBit >> bitPos) & 1
+		if currDIVBit == 1 && prevBit == 0 { //"rising edge" (meaning going from 0 to 1)
 			t.TIMA++
-			if t.TIMA == 0 { //overflow
+			if t.TIMA == 0xFF { //overflow
 				t.TIMA = t.TMA               //reload
 				cpu.Memory[0xFF0F] |= 1 << 2 //bit 2 of the Interrupt Flag Register at $FF0F is set
 			}
 		}
+		//t.oldStoredDivBit = t.div
 		t.oldStoredDivBit = byte(currDIVBit)
 	}
+
+	//div -ncremented every 256 tCycles
+	//t.div += uint16(tCycles)
+	//if t.div >= 256 {
+	//	t.div -= 256
+	//}
+	//if t.TAC&(1<<2) == 0 { //timer disabled
+	//	return
+	//}
+	//var rate int
+	//// Bits 1-0 : Clock Select
+	//switch t.TAC & 0b0011 {
+	//case 0b00:
+	//	rate = 1024
+	//case 0b01:
+	//	rate = 16
+	//case 0b10:
+	//	rate = 64
+	//case 0b11:
+	//	rate = 256
+	//}
+	//if t.TAC&(1<<2) != 0 { //timer enabled
+	//	currDIVBit := t.div & uint16(rate)
+	//
+	//	if currDIVBit == 1 && t.oldStoredDivBit == 0 { //"rising edge" (meaning going from 0 to 1)
+	//		t.TIMA++
+	//		if t.TIMA == 0 { //overflow
+	//			t.TIMA = t.TMA               //reload
+	//			cpu.Memory[0xFF0F] |= 1 << 2 //bit 2 of the Interrupt Flag Register at $FF0F is set
+	//		}
+	//	}
+	//	t.oldStoredDivBit = byte(currDIVBit)
+	//}
 
 }
